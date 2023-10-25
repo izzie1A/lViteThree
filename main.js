5e79fea261f961967472f867a7a9e2f476ee69ef
izzie1A <47637788+izzie1A@users.noreply.github.com>
2023-10-25 06:11:44 +0100

    fist render

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.css'
+import * as THREE from 'three'
+
+console.log('tests')
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene,camera);
+// complete Render// Torus
+
